73e2f64206a3ffc91045b772c0b937113fdc6288
Axhu99 <xhuliodreni@icloud.com>
2024-03-13 17:51:33 +0100

    adde header and jumbotron

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>{{ env('APP_NAME') }} | @yield('title')</title>
+    <title>{{ env('APP_NAME') }} - @yield('title')</title>
 
     <link rel="icon" href="{{ asset('images/dc-logo.png') }}" type="images/png">
     @vite('resources/js/app.js')
@@ -31,6 +31,7 @@
                 <li><a href="">SHOP</a></li>
             </ul>
         </nav>
+        <div class="jumbotron"></div>
     </header>
     {{-- MAIN --}}
     <main>
